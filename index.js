19aedc1c9da5d40c203aedf0fd2615f4d801b089
nicoledeklerk <nicolenataliedeklerk@gmail.com>
2025-03-14 10:32:25 +0200

    Validate Data Inputs

--- index.js
+++ index.js
@@ -5,13 +5,26 @@
  * 3. Make these calculations robust such that the calculation does not give an incorrect result, it throws an error to the user if something has gone wrong (parameter used with an incorrect unit of measurement, etc)
  */
 
-// Given Parameters
+// Given Parameters //
 const initialVelocity = 10000; // velocity (km/h)
 const acceleration = 3; // acceleration (m/s^2)
 const timeSecondsPerHour = 3600; // seconds (1 hour)
 const initialDistance = 0; // distance (km)
 const remainingFuel = 5000; // remaining fuel (kg)
 const fuelBurnRate = 0.5; // fuel burn rate (kg/s)
+
+
+function validateParameters() {
+  if (typeof initialVelocity !== 'number' || initialVelocity < 0 ||
+      typeof acceleration !== 'number' || acceleration < 0 ||
+      typeof timeSecondsPerHour !== 'number' || timeSecondsPerHour !== 3600 ||
+      typeof remainingFuel !== 'number' || remainingFuel < 0 ||
+      typeof fuelBurnRate !== 'number' || fuelBurnRate < 0) {
+      throw new Error('Invalid input parameters');
+  }
+}
+
+validateParameters();
 
 const correctedNewVelocity = calcNewVelocity(initialVelocity, acceleration, timeSecondsPerHour); //calculates new velocity based on acceleration
 const correctedNewDistance = initialDistance + (initialVelocity * (timeSecondsPerHour / 3600)) + (0.5 * acceleration * Math.pow(timeSecondsPerHour / 3600, 2) / 1000); //calcultes new distance
@@ -26,7 +39,7 @@ function calcNewVelocity(initialVelocity, acceleration, timeSecondsPerHour) {
 
 console.log(`Corrected New Velocity: ${correctedNewVelocity} km/h`);
 console.log(`Corrected New Distance: ${correctedNewDistance} km`);
-console.log(`Corrected Remaining Fuel: ${correctedRemainingFuel} kg`);
+console.log(`Corrected Remaining Fuel: ${correctedRemainingFuel} kg`); 
 
 
 
